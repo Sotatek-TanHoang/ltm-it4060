66365448765392d39898384583011d3183096863
Sotatek-TanHoang <tan.hoang@sotatek.com>
2022-07-27 06:24:32 +0700

    fix conflict

--- Server/Util.h
+++ Server/Util.h
@@ -18,16 +18,11 @@ string adminStartRoom(string roomName, Request* req);
 string adminEndGame(string roomName, Request* req);
 string adminDeleteRoom(string roomName, Request* req);
 //phu
-<<<<<<< HEAD
 string userGetRooms(string payload, Request * req);
 string playerJoinRoom(string payload, Request* req);
 string playerQuitRoom(string payload, Request* req);
 string playerAnswerQuizz(string payload, Request* req);
 
-=======
-string userGetRoom(Request *req);
-string userJoinRoom(string roomName, Request *req);
->>>>>>> 344db0b9d1f9dc8041188df49faa4959072ca5af
 bool processData(char* buff, char* queue, Request* req);
 
 void appendQueue(char* buff, char* queue);
